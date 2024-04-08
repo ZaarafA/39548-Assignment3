7b028384ea9b101c769fb3c6acd6e8bc99cf6bad
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-04-08 00:03:38 -0400

    Background and Content Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,20 +10,21 @@ import {Link} from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <img src="https://picsum.photos/200/200" alt="bank"/>
+      <div className='Homepage'>
+        {/* <img src="https://picsum.photos/200/200" alt="bank"/> */}
 
-        <h1>Bank of React</h1>
-
-        <Link to="/userProfile">User Profile</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="/credits">Credits</Link>
-        <br/>
-        <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
-        <br/><br/>
-        <AccountBalance accountBalance={this.props.accountBalance}/>
+        <div class="content">
+          <h1>Bank of React</h1>
+          <Link to="/userProfile">User Profile</Link>
+          <br/>
+          <Link to="/login">Login</Link>
+          <br/>
+          <Link to="/credits">Credits</Link>
+          <br/>
+          <Link to="/debits">Debits</Link>
+          <br/><br/>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
       </div>
     );
   }
